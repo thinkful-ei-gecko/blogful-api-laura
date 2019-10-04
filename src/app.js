854fcf3324b45af0d01b253b426cbd592fed8e90
laura <laura.j.elias@gmail.com>
2019-10-04 16:39:51 -0500

    checkpoint 16 blogful

---
+++ src/app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config.js');
+const articlesRouter = require('./articles/articles-router.js');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.use('/articles', articlesRouter);
+
+app.get('/', (req, res) => { res.send('Hello, world!'); });
+ 
+/* app.get('/xss', (req, res) => {
+  res.cookie('secretToken', '1234567890');
+  res.sendFile(__dirname + '/xss-example.html');
+}); */
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    console.error(error);
+     response = { error: { message: 'server error' }};
+  } else {
+    console.error(error);
+    response = { error, message: error.message };
+   }
+   res.status(500).json(response);
+ });
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+const app = require('./app.js');
+const { PORT, DB_URL } = require('./config.js');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
